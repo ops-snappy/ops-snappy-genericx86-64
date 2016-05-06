fdf8f825e2e55dfd5ea845565c3c49af04f86f41
Mike Berger <mabnhdev@gmail.com>
2016-05-06 15:32:10 -0400

    Base snap on pre-built OpenEmbedded build of OpenSwitch.

--- parts/plugins/x_openswitch.py
+++ parts/plugins/x_openswitch.py
@@ -1,8 +1,10 @@
 import snapcraft
-import snapcraft.common
 import os
+import glob
 import logging
 import shutil
+
+from snapcraft.internal import common, sources
 
 logger = logging.getLogger(__name__)
 
@@ -12,19 +14,47 @@ class XOpenSwitchPlugin(snapcraft.BasePlugin):
         schema = super().schema()
         schema['properties']['platform'] = {
             'type': 'string',
-            'default': 'as6712',
         }
+        schema['properties']['kernelversion'] = {
+            'type': 'string',
+        }
+        schema['properties']['cdppart'] = {
+            'type': 'string',
+        }
+        schema['required'].append('platform')
         return schema
 
     def __init__(self, name, options, project):
         super().__init__(name, options, project)
-        logger.info('__init__ override...')
+
+        """ With pre-built, make sure configured platform matches yaml file.
+        """
         self.build_packages.append('make')
 
-    def clean_pull(self):
-        logger.info('clean_pull override NOP')
-        """ Don't clean the source dir for now since we are pre-building.
-        """
+        self.downloaddir = os.path.join(self.partdir, 'download')
+        self.headersdir = os.path.join(self.downloaddir, 'linux-headers')
+        if self.options.cdppart:
+            self.cdpdir = os.path.join(project.parts_dir, self.options.cdppart)
+            if self.options.kernelversion:
+                self.downloaddir = os.path.join(self.partdir, 'download')
+                self.headersdir = os.path.join(self.downloaddir, 'linux-headers')
+            else:
+                raise ValueError('CDP requires kernelversion')
+        else:
+            self.cdpdir = None
+                
+
+    def pull(self):
+        super().pull()
+        if self.cdpdir != None:
+            if os.path.exists(self.downloaddir):
+                shutil.rmtree(self.downloaddir)
+            os.mkdir(self.downloaddir)
+            os.mkdir(self.headersdir)
+            self.run(['apt', 'download', 'linux-headers-' + self.options.kernelversion], cwd=self.downloaddir)
+            debfiles = glob.glob(os.path.join(self.downloaddir, '*.deb'))
+            self.run(['dpkg', '-x'] + debfiles + [self.headersdir])
+            self.run(['rm', '-f'] + debfiles)
 
     def build(self):
 
@@ -38,10 +68,24 @@ class XOpenSwitchPlugin(snapcraft.BasePlugin):
                 shutil.rmtree(self.build_basedir)
                 os.symlink(self.sourcedir, self.build_basedir)
 
+        """Make sure the pre-built image was configured as expected.
+        """
+        with open(os.path.join(self.sourcedir, '.platform'), 'r') as platformfile:
+            configuredplatform = platformfile.read().replace('\n','');
+        if self.options.platform != configuredplatform:
+            raise ValueError('Source is configured for ' + configuredplatform + '; expected ' + self.options.platform)
+
+        """Must rebuild the opennsl for the target system
+        """
+        if self.cdpdir != None:
+            self.run(['make', 'host-opennsl', 'CDPDIR=' + self.cdpdir, 'LINUX_HEADERS=' + self.headersdir, 'KERNEL_VERSION=' + self.options.kernelversion])
+
         """ Since we are pre-building, just have to install into installdir
         """
-        logger.info('build override make install DESTDIR=' + self.build_basedir)
-        self.run(command + ['install-snappy', 'DESTDIR=' + self.installdir])
+        if self.cdpdir != None:
+            self.run(command + ['install-snappy', 'DESTDIR=' + self.installdir, 'CDPDIR=' + self.cdpdir])
+        else:
+            self.run(command + ['install-snappy', 'DESTDIR=' + self.installdir])
 
     def clean_build(self):
 
@@ -54,5 +98,6 @@ class XOpenSwitchPlugin(snapcraft.BasePlugin):
             shutil.rmtree(self.build_basedir)
 
         if os.path.exists(self.installdir):
-            logger.info('rmtree ' + self.installdir)
             shutil.rmtree(self.installdir)
+        if os.path.exists(self.downloaddir):
+            shutil.rmtree(self.downloaddir)
